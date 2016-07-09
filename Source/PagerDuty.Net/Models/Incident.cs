18bc131c4ddaac48f6fca707df87c67c31a46d75
Brian Richards <Brian.Richards@emerson.com>
2016-07-08 20:25:55 -0500

    Adding TriggerSummaryData where it needs to be [BR]

--- Source/PagerDuty.Net/Models/Incident.cs
+++ Source/PagerDuty.Net/Models/Incident.cs
@@ -17,8 +17,9 @@ namespace PagerDuty.Net {
         public User assigned_to_user { get; set; }
         public User last_status_change_by { get; set; }
         public DateTime last_status_change_on { get; set; }
-        public string trigger_summary_data { get; set; }
+        public TriggerSummaryData trigger_summary_data { get; set; }
         public string trigger_details_html_url { get; set; }
         public int number_of_escalations { get; set; }
     }
+
 }
--- Source/PagerDuty.Net/Models/TriggerSummaryData.cs
+++ Source/PagerDuty.Net/Models/TriggerSummaryData.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PagerDuty.Net {
     [Serializable()]
     public class TriggerSummaryData {
-        public string subject { get; set; }
+        public string client { get; set; }
         public string description { get; set; }
     }
 }
